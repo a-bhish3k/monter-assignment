846d4317e8ba127451d1c5f97e11e703967583a3
a-bhish3k <abhishekumar692@gmail.com>
2024-04-19 20:13:08 +0530

    reports-table and pagination component added

--- app/reports/page.tsx
+++ app/reports/page.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
+import { fetchReports } from "../lib/data";
+import ReportsTable from "../ui/reports/table";
+import Pagination from "../ui/reports/pagination";
 
-export default function Reports() {
+export default async function Reports() {
+  const reports = await fetchReports();
+
   return (
     <div className="min-h-screen text-neutral-800 flex items-center justify-center">
       <div className="h-[calc(100vh-48px)] w-[calc(100vw-48px)] border rounded-lg shadow-lg">
@@ -41,48 +46,11 @@ export default function Reports() {
             </div>
           </div>
           <div className="h-full overflow-auto">
-            <table className="table-auto min-w-full">
-              <thead className="bg-neutral-100">
-                <tr className="text-sm text-left text-neutral-600">
-                  <th className="py-2 ps-6">Date</th>
-                  <th>Report Name</th>
-                  <th className="text-right pe-6">Download</th>
-                </tr>
-              </thead>
-              <tbody className="text-lg">
-                <tr>
-                  <td className="py-2 ps-6 leading-6">
-                    27.01.2021 <br />
-                    <span className="text-sm font-bold text-neutral-500">
-                      12:25 PM
-                    </span>
-                  </td>
-                  <td className="align-top pt-2">
-                    App_url_somw_now_ajfdka;skdsa.csv
-                  </td>
-                  <td className="pe-10">
-                    <a href="/favicon.ico" download>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="size-7 ml-auto"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m.75 12 3 3m0 0 3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
-                        />
-                      </svg>
-                    </a>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+            <ReportsTable reports={reports} />
           </div>
-          <div className="py-4 border-t">hello</div>
+          <div className="py-4 border-t">
+            <Pagination />
+          </div>
         </div>
       </div>
     </div>
