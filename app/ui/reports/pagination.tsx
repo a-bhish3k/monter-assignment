77e08bdb7ed2decd8e11376a3860e06d65b261b9
a-bhish3k <abhishekumar692@gmail.com>
2024-04-20 14:57:50 +0530

    icons added

--- app/ui/reports/pagination.tsx
+++ app/ui/reports/pagination.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { usePathname, useSearchParams } from "next/navigation";
+import { ArrowLeftIcon, ArrowRightIcon } from "../icons";
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
   const pathname = usePathname();
@@ -23,18 +24,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
             currentPage <= 1 && "pointer-events-none"
           } flex items-center gap-1`}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="w-5 h-5"
-          >
-            <path
-              fillRule="evenodd"
-              d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z"
-              clipRule="evenodd"
-            />
-          </svg>
+          <ArrowLeftIcon />
           <span className=" text-sm">Prev</span>
         </Link>
       </li>
@@ -62,18 +52,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
           } flex items-center gap-1`}
         >
           <span className=" text-sm">Next</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="w-5 h-5"
-          >
-            <path
-              fillRule="evenodd"
-              d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
-              clipRule="evenodd"
-            />
-          </svg>
+          <ArrowRightIcon />
         </Link>
       </li>
     </ul>
