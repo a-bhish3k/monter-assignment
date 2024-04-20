93ef5861502c896e53d11dfb9eb97939e476dce8
a-bhish3k <abhishekumar692@gmail.com>
2024-04-20 14:56:17 +0530

    home page metadata updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Monter | Reports",
-  description: "Recently generated reports",
+  title: "Monter | Home",
+  description: "Homepage of monter",
 };
 
 export default function RootLayout({
