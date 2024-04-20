7fe24f106e8be6005324a8079274c58f44f48844
a-bhish3k <abhishekumar692@gmail.com>
2024-04-20 17:48:40 +0530

    design-fix for mobile-devices

--- app/ui/reports/table.tsx
+++ app/ui/reports/table.tsx
@@ -6,24 +6,24 @@ export default function ReportsTable({ reports }: { reports: Report[] }) {
     <table className="table-auto w-full">
       <thead className="bg-neutral-100 sticky top-0">
         <tr className="text-sm text-left text-neutral-600">
-          <th className="py-2 ps-6">Date</th>
+          <th className="py-2 ps-3 sm:ps-6">Date</th>
           <th>Report Name</th>
-          <th className="text-right pe-6">Download</th>
+          <th className="text-right pe-3 sm:pe-6">Download</th>
         </tr>
       </thead>
       <tbody className="text-sm sm:text-base lg:text-lg">
         {reports?.map((item) => (
           <tr key={item.id}>
-            <td className="py-2 px-6 leading-6">
+            <td className="py-2 px-3 sm:px-6 leading-6">
               {item.date} <br />
               <span className="text-sm font-bold text-neutral-500">
                 {item.time}
               </span>
             </td>
-            <td className="align-top pt-2 pe-6 text-wrap">
+            <td className="align-top py-2 pt-2 sm:pe-4 break-all">
               {item.report_name}
             </td>
-            <td className="pe-10">
+            <td className="pe-8 sm:pe-10">
               <a href={item.download_link} download>
                 <DownloadIcon />
               </a>
